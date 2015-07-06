6ead3c8f293815497f54864a461d7bebb443c12b
Kevin Gu <kevingu@Charless-MacBook-Pro.local>
2015-07-06 10:21:33 -0700

    added basic ui for my task

--- app/controllers/upload_crm_controller.rb
+++ app/controllers/upload_crm_controller.rb
@@ -1,6 +1,6 @@
 class UploadCrmController < ApplicationController
   def index
-    #Code for running pyhton script
+    #Code for running python script
     #test=`python Rails.root.join('bin','test.py')`
     #puts "TEST Python script"
     #puts test
@@ -8,10 +8,12 @@ class UploadCrmController < ApplicationController
 
   def upload
     uploaded_io = params[:upl]
-    print "UPLoading"
     File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
       file.write(uploaded_io.read)
     end
+    #test=`python Rails.root.join('bin','test.py')`
+    #puts "TEST Python script"
+    #puts test
     redirect_to invalid_entry_index_path, notice: "File uploaded"
   end
 
