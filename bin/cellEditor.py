93d9c1f9db6854ff90269f09db483a31121e538d
Richard Tran <RichardTran93@gmail.com>
2015-07-07 15:17:06 -0700

    celleditor broken

--- bin/cellEditor.py
+++ bin/cellEditor.py
@@ -1,19 +1,42 @@
+import xlrd
 import sys
 from editpyxl import Workbook
 import ast
-########################USAGE################
-# python cellEditor.py [filename] [[row1, column1, 123], ..., [row2, column2, 456]]
-#############################################
+from openpyxl import load_workbook
+
+def findRows(fileName, ids):
+    wb = load_workbook(filename=fileName, read_only = True)
+    ws = wb['PipelineView']
+    count = 1
+    rows = []
+    for row in ws.rows:
+        if row[0].value in ids:
+            rows.append(count)
+        count += 1
+    return rows
+
+#
+# ids = ['AUIA-HFA4A', 'AUIA-HGZ3G', 'AUIA-HHBUO']
+# findRows('b.xlsm', ids)
 args = sys.argv
 wb = Workbook()
 fileName = args[1]
 strs = args[2]
+strs = strs.split()
 wb.open(fileName)
 ws = wb.active
-data = [[i for i in x.strip(" []").split(",")] for x in strs.strip('[]').split("],")]
-for d in data:
-    row = d[0]
-    col = d[1]
-    value = d[2]
-    ws.cell(col+row).value = value
+
+######find the rows#########
+rows = []
+cols = []
+changes = []
+for s in strs:
+    rows.append(s[0])
+    cols.append(s[1])
+    changes.append(s[2])
+rows = findRows(fileName, rows)
+
+#######edit cells#########
+for i in range(len(rows)): # for all of the changes
+    ws.cell(cols[i] + rows[i]).value = changes[i]
 wb.save(fileName)
