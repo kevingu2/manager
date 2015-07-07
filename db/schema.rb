3d77b2d4a64cc944463f19f3ccdcb7a93ce74e1e
Ryan Milkovits <mrmonkey_888@yahoo.com>
2015-07-07 11:39:08 -0700

    favicon redundancy for different browsers

--- db/schema.rb
+++ db/schema.rb
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20150707182754) do
     t.string   "role"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "remember_digest"
   end
 
 end
