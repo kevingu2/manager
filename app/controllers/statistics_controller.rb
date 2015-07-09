6246936d3f9d2a1d0b3eafab60867aff84d0eeab
Ryan Milkovits <mrmonkey_888@yahoo.com>
2015-07-09 15:34:51 -0700

    year selector now works on stats page

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -2,10 +2,16 @@
 
 class StatisticsController < ApplicationController
   def index
-  	# a zero-initialized array to hold the RFP counts for each month
+  	# a zero-initialized array to hold the RFP counts for months for each year
   	@rfpMonths = Array.new(12,0)
-    # a zero-initialized array to hold the mil counts for each month
+    @rfpMonths2014 = Array.new(12,0)
+    @rfpMonths2015 = Array.new(12,0)
+    @rfpMonths2016 = Array.new(12,0)
+    # a zero-initialized array to hold the mil counts for months for each year
     @milMonths = Array.new(12,0)
+    @milMonths2014 = Array.new(12,0)
+    @milMonths2015 = Array.new(12,0)
+    @milMonths2016 = Array.new(12,0) 
 
   	#get all Oppty objects from database into the @oppties collection
   	@oppties = Oppty.all
@@ -14,6 +20,7 @@ class StatisticsController < ApplicationController
   	@oppties.each do |opp|
   		#get month of this particular opportunity
   		month = opp.rfpDate.mon
+      year = opp.rfpDate.year
 
   		#fill up the rfpMonths array 
   		if month == 1 then @rfpMonths[0]+=1 end
@@ -28,6 +35,45 @@ class StatisticsController < ApplicationController
   		if month == 10 then @rfpMonths[9]+=1 end
   		if month == 11 then @rfpMonths[10]+=1 end
   		if month == 12 then @rfpMonths[11]+=1 end
+
+      if month == 1 and year == 2014 then @rfpMonths2014[0]+=1 end
+      if month == 2 and year == 2014 then @rfpMonths2014[1]+=1 end
+      if month == 3 and year == 2014 then @rfpMonths2014[2]+=1 end
+      if month == 4 and year == 2014 then @rfpMonths2014[3]+=1 end
+      if month == 5 and year == 2014 then @rfpMonths2014[4]+=1 end
+      if month == 6 and year == 2014 then @rfpMonths2014[5]+=1 end
+      if month == 7 and year == 2014 then @rfpMonths2014[6]+=1 end
+      if month == 8 and year == 2014 then @rfpMonths2014[7]+=1 end
+      if month == 9 and year == 2014 then @rfpMonths2014[8]+=1 end
+      if month == 10 and year == 2014 then @rfpMonths2014[9]+=1 end
+      if month == 11 and year == 2014 then @rfpMonths2014[10]+=1 end
+      if month == 12 and year == 2014 then @rfpMonths2014[11]+=1 end
+
+      if month == 1 and year == 2015 then @rfpMonths2015[0]+=1 end
+      if month == 2 and year == 2015 then @rfpMonths2015[1]+=1 end
+      if month == 3 and year == 2015 then @rfpMonths2015[2]+=1 end
+      if month == 4 and year == 2015 then @rfpMonths2015[3]+=1 end
+      if month == 5 and year == 2015 then @rfpMonths2015[4]+=1 end
+      if month == 6 and year == 2015 then @rfpMonths2015[5]+=1 end
+      if month == 7 and year == 2015 then @rfpMonths2015[6]+=1 end
+      if month == 8 and year == 2015 then @rfpMonths2015[7]+=1 end
+      if month == 9 and year == 2015 then @rfpMonths2015[8]+=1 end
+      if month == 10 and year == 2015 then @rfpMonths2015[9]+=1 end
+      if month == 11 and year == 2015 then @rfpMonths2015[10]+=1 end
+      if month == 12 and year == 2015 then @rfpMonths2015[11]+=1 end
+
+      if month == 1 and year == 2016 then @rfpMonths2016[0]+=1 end
+      if month == 2 and year == 2016 then @rfpMonths2016[1]+=1 end
+      if month == 3 and year == 2016 then @rfpMonths2016[2]+=1 end
+      if month == 4 and year == 2016 then @rfpMonths2016[3]+=1 end
+      if month == 5 and year == 2016 then @rfpMonths2016[4]+=1 end
+      if month == 6 and year == 2016 then @rfpMonths2016[5]+=1 end
+      if month == 7 and year == 2016 then @rfpMonths2016[6]+=1 end
+      if month == 8 and year == 2016 then @rfpMonths2016[7]+=1 end
+      if month == 9 and year == 2016 then @rfpMonths2016[8]+=1 end
+      if month == 10 and year == 2016 then @rfpMonths2016[9]+=1 end
+      if month == 11 and year == 2016 then @rfpMonths2016[10]+=1 end
+      if month == 12 and year == 2016 then @rfpMonths2016[11]+=1 end
   	end
   	
   	#get all Oppty objects from database into the @oppties collection
@@ -37,6 +83,7 @@ class StatisticsController < ApplicationController
   	@histories.each do |hist|
   		#get month of this particular opportunity
   		month = hist.rfpDate.mon
+      year = hist.rfpDate.year
 
   		#fill up the rfpMonths array 
   		if month == 1 then @rfpMonths[0]+=1 end
@@ -51,14 +98,54 @@ class StatisticsController < ApplicationController
   		if month == 10 then @rfpMonths[9]+=1 end
   		if month == 11 then @rfpMonths[10]+=1 end
   		if month == 12 then @rfpMonths[11]+=1 end
+
+      if month == 1 and year == 2014 then @rfpMonths2014[0]+=1 end
+      if month == 2 and year == 2014 then @rfpMonths2014[1]+=1 end
+      if month == 3 and year == 2014 then @rfpMonths2014[2]+=1 end
+      if month == 4 and year == 2014 then @rfpMonths2014[3]+=1 end
+      if month == 5 and year == 2014 then @rfpMonths2014[4]+=1 end
+      if month == 6 and year == 2014 then @rfpMonths2014[5]+=1 end
+      if month == 7 and year == 2014 then @rfpMonths2014[6]+=1 end
+      if month == 8 and year == 2014 then @rfpMonths2014[7]+=1 end
+      if month == 9 and year == 2014 then @rfpMonths2014[8]+=1 end
+      if month == 10 and year == 2014 then @rfpMonths2014[9]+=1 end
+      if month == 11 and year == 2014 then @rfpMonths2014[10]+=1 end
+      if month == 12 and year == 2014 then @rfpMonths2014[11]+=1 end
+
+      if month == 1 and year == 2015 then @rfpMonths2015[0]+=1 end
+      if month == 2 and year == 2015 then @rfpMonths2015[1]+=1 end
+      if month == 3 and year == 2015 then @rfpMonths2015[2]+=1 end
+      if month == 4 and year == 2015 then @rfpMonths2015[3]+=1 end
+      if month == 5 and year == 2015 then @rfpMonths2015[4]+=1 end
+      if month == 6 and year == 2015 then @rfpMonths2015[5]+=1 end
+      if month == 7 and year == 2015 then @rfpMonths2015[6]+=1 end
+      if month == 8 and year == 2015 then @rfpMonths2015[7]+=1 end
+      if month == 9 and year == 2015 then @rfpMonths2015[8]+=1 end
+      if month == 10 and year == 2015 then @rfpMonths2015[9]+=1 end
+      if month == 11 and year == 2015 then @rfpMonths2015[10]+=1 end
+      if month == 12 and year == 2015 then @rfpMonths2015[11]+=1 end
+
+      if month == 1 and year == 2016 then @rfpMonths2016[0]+=1 end
+      if month == 2 and year == 2016 then @rfpMonths2016[1]+=1 end
+      if month == 3 and year == 2016 then @rfpMonths2016[2]+=1 end
+      if month == 4 and year == 2016 then @rfpMonths2016[3]+=1 end
+      if month == 5 and year == 2016 then @rfpMonths2016[4]+=1 end
+      if month == 6 and year == 2016 then @rfpMonths2016[5]+=1 end
+      if month == 7 and year == 2016 then @rfpMonths2016[6]+=1 end
+      if month == 8 and year == 2016 then @rfpMonths2016[7]+=1 end
+      if month == 9 and year == 2016 then @rfpMonths2016[8]+=1 end
+      if month == 10 and year == 2016 then @rfpMonths2016[9]+=1 end
+      if month == 11 and year == 2016 then @rfpMonths2016[10]+=1 end
+      if month == 12 and year == 2016 then @rfpMonths2016[11]+=1 end
   	end
 
-    #at this point @milMonths has the RFP counts for each month
+    #at this point @rfpMonths has the RFP counts for each month
 
     #iterate through all of the oppurtunities
     @oppties.each do |opp|
       #get month of this particular opportunity
-      month = opp.rfpDate.mon
+      month = opp.awardDate.mon
+      year = opp.awardDate.year
 
       #fill up the milMonths array 
       unless opp.value == nil then
@@ -74,16 +161,53 @@ class StatisticsController < ApplicationController
         if month == 10 then @milMonths[9]+=opp.value end
         if month == 11 then @milMonths[10]+=opp.value end
         if month == 12 then @milMonths[11]+=opp.value end
+
+        if month == 1 and year == 2014 then @milMonths2014[0]+=opp.value end
+        if month == 2 and year == 2014 then @milMonths2014[1]+=opp.value end
+        if month == 3 and year == 2014 then @milMonths2014[2]+=opp.value end
+        if month == 4 and year == 2014 then @milMonths2014[3]+=opp.value end
+        if month == 5 and year == 2014 then @milMonths2014[4]+=opp.value end
+        if month == 6 and year == 2014 then @milMonths2014[5]+=opp.value end
+        if month == 7 and year == 2014 then @milMonths2014[6]+=opp.value end
+        if month == 8 and year == 2014 then @milMonths2014[7]+=opp.value end
+        if month == 9 and year == 2014 then @milMonths2014[8]+=opp.value end
+        if month == 10 and year == 2014 then @milMonths2014[9]+=opp.value end
+        if month == 11 and year == 2014 then @milMonths2014[10]+=opp.value end
+        if month == 12 and year == 2014 then @milMonths2014[11]+=opp.value end
+
+        if month == 1 and year == 2015 then @milMonths2015[0]+=opp.value end
+        if month == 2 and year == 2015 then @milMonths2015[1]+=opp.value end
+        if month == 3 and year == 2015 then @milMonths2015[2]+=opp.value end
+        if month == 4 and year == 2015 then @milMonths2015[3]+=opp.value end
+        if month == 5 and year == 2015 then @milMonths2015[4]+=opp.value end
+        if month == 6 and year == 2015 then @milMonths2015[5]+=opp.value end
+        if month == 7 and year == 2015 then @milMonths2015[6]+=opp.value end
+        if month == 8 and year == 2015 then @milMonths2015[7]+=opp.value end
+        if month == 9 and year == 2015 then @milMonths2015[8]+=opp.value end
+        if month == 10 and year == 2015 then @milMonths2015[9]+=opp.value end
+        if month == 11 and year == 2015 then @milMonths2015[10]+=opp.value end
+        if month == 12 and year == 2015 then @milMonths2015[11]+=opp.value end
+
+        if month == 1 and year == 2016 then @milMonths2016[0]+=opp.value end
+        if month == 2 and year == 2016 then @milMonths2016[1]+=opp.value end
+        if month == 3 and year == 2016 then @milMonths2016[2]+=opp.value end
+        if month == 4 and year == 2016 then @milMonths2016[3]+=opp.value end
+        if month == 5 and year == 2016 then @milMonths2016[4]+=opp.value end
+        if month == 6 and year == 2016 then @milMonths2016[5]+=opp.value end
+        if month == 7 and year == 2016 then @milMonths2016[6]+=opp.value end
+        if month == 8 and year == 2016 then @milMonths2016[7]+=opp.value end
+        if month == 9 and year == 2016 then @milMonths2016[8]+=opp.value end
+        if month == 10 and year == 2016 then @milMonths2016[9]+=opp.value end
+        if month == 11 and year == 2016 then @milMonths2016[10]+=opp.value end
+        if month == 12 and year == 2016 then @milMonths2016[11]+=opp.value end
       end
     end
-    
-    #get all Oppty objects from database into the @oppties collection
-    @histories = History.all
 
     #iterate through all of the oppurtunities
     @histories.each do |hist|
       #get month of this particular opportunity
       month = hist.awardDate.mon
+      year = hist.awardDate.year
 
       #fill up the milMonths array 
       unless hist.value == nil then
@@ -99,6 +223,45 @@ class StatisticsController < ApplicationController
         if month == 10 then @milMonths[9]+=hist.value end
         if month == 11 then @milMonths[10]+=hist.value end
         if month == 12 then @milMonths[11]+=hist.value end
+
+        if month == 1 and year == 2014 then @milMonths2014[0]+=hist.value end
+        if month == 2 and year == 2014 then @milMonths2014[1]+=hist.value end
+        if month == 3 and year == 2014 then @milMonths2014[2]+=hist.value end
+        if month == 4 and year == 2014 then @milMonths2014[3]+=hist.value end
+        if month == 5 and year == 2014 then @milMonths2014[4]+=hist.value end
+        if month == 6 and year == 2014 then @milMonths2014[5]+=hist.value end
+        if month == 7 and year == 2014 then @milMonths2014[6]+=hist.value end
+        if month == 8 and year == 2014 then @milMonths2014[7]+=hist.value end
+        if month == 9 and year == 2014 then @milMonths2014[8]+=hist.value end
+        if month == 10 and year == 2014 then @milMonths2014[9]+=hist.value end
+        if month == 11 and year == 2014 then @milMonths2014[10]+=hist.value end
+        if month == 12 and year == 2014 then @milMonths2014[11]+=hist.value end
+
+        if month == 1 and year == 2015 then @milMonths2015[0]+=hist.value end
+        if month == 2 and year == 2015 then @milMonths2015[1]+=hist.value end
+        if month == 3 and year == 2015 then @milMonths2015[2]+=hist.value end
+        if month == 4 and year == 2015 then @milMonths2015[3]+=hist.value end
+        if month == 5 and year == 2015 then @milMonths2015[4]+=hist.value end
+        if month == 6 and year == 2015 then @milMonths2015[5]+=hist.value end
+        if month == 7 and year == 2015 then @milMonths2015[6]+=hist.value end
+        if month == 8 and year == 2015 then @milMonths2015[7]+=hist.value end
+        if month == 9 and year == 2015 then @milMonths2015[8]+=hist.value end
+        if month == 10 and year == 2015 then @milMonths2015[9]+=hist.value end
+        if month == 11 and year == 2015 then @milMonths2015[10]+=hist.value end
+        if month == 12 and year == 2015 then @milMonths2015[11]+=hist.value end
+
+        if month == 1 and year == 2016 then @milMonths2016[0]+=hist.value end
+        if month == 2 and year == 2016 then @milMonths2016[1]+=hist.value end
+        if month == 3 and year == 2016 then @milMonths2016[2]+=hist.value end
+        if month == 4 and year == 2016 then @milMonths2016[3]+=hist.value end
+        if month == 5 and year == 2016 then @milMonths2016[4]+=hist.value end
+        if month == 6 and year == 2016 then @milMonths2016[5]+=hist.value end
+        if month == 7 and year == 2016 then @milMonths2016[6]+=hist.value end
+        if month == 8 and year == 2016 then @milMonths2016[7]+=hist.value end
+        if month == 9 and year == 2016 then @milMonths2016[8]+=hist.value end
+        if month == 10 and year == 2016 then @milMonths2016[9]+=hist.value end
+        if month == 11 and year == 2016 then @milMonths2016[10]+=hist.value end
+        if month == 12 and year == 2016 then @milMonths2016[11]+=hist.value end
       end
     end
   	
@@ -116,6 +279,45 @@ class StatisticsController < ApplicationController
     @milMonths[9] = ( @milMonths[9] * 10.0).round / 10.0
     @milMonths[10] = ( @milMonths[10] * 10.0).round / 10.0
     @milMonths[11] = ( @milMonths[11] * 10.0).round / 10.0
+
+    @milMonths2014[0] = ( @milMonths2014[0] * 10.0).round / 10.0
+    @milMonths2014[1] = ( @milMonths2014[1] * 10.0).round / 10.0
+    @milMonths2014[2] = ( @milMonths2014[2] * 10.0).round / 10.0
+    @milMonths2014[3] = ( @milMonths2014[3] * 10.0).round / 10.0
+    @milMonths2014[4] = ( @milMonths2014[4] * 10.0).round / 10.0
+    @milMonths2014[5] = ( @milMonths2014[5] * 10.0).round / 10.0
+    @milMonths2014[6] = ( @milMonths2014[6] * 10.0).round / 10.0
+    @milMonths2014[7] = ( @milMonths2014[7] * 10.0).round / 10.0
+    @milMonths2014[8] = ( @milMonths2014[8] * 10.0).round / 10.0
+    @milMonths2014[9] = ( @milMonths2014[9] * 10.0).round / 10.0
+    @milMonths2014[10] = ( @milMonths2014[10] * 10.0).round / 10.0
+    @milMonths2014[11] = ( @milMonths2014[11] * 10.0).round / 10.0
+
+    @milMonths2015[0] = ( @milMonths2015[0] * 10.0).round / 10.0
+    @milMonths2015[1] = ( @milMonths2015[1] * 10.0).round / 10.0
+    @milMonths2015[2] = ( @milMonths2015[2] * 10.0).round / 10.0
+    @milMonths2015[3] = ( @milMonths2015[3] * 10.0).round / 10.0
+    @milMonths2015[4] = ( @milMonths2015[4] * 10.0).round / 10.0
+    @milMonths2015[5] = ( @milMonths2015[5] * 10.0).round / 10.0
+    @milMonths2015[6] = ( @milMonths2015[6] * 10.0).round / 10.0
+    @milMonths2015[7] = ( @milMonths2015[7] * 10.0).round / 10.0
+    @milMonths2015[8] = ( @milMonths2015[8] * 10.0).round / 10.0
+    @milMonths2015[9] = ( @milMonths2015[9] * 10.0).round / 10.0
+    @milMonths2015[10] = ( @milMonths2015[10] * 10.0).round / 10.0
+    @milMonths2015[11] = ( @milMonths2015[11] * 10.0).round / 10.0
+
+    @milMonths2016[0] = ( @milMonths2016[0] * 10.0).round / 10.0
+    @milMonths2016[1] = ( @milMonths2016[1] * 10.0).round / 10.0
+    @milMonths2016[2] = ( @milMonths2016[2] * 10.0).round / 10.0
+    @milMonths2016[3] = ( @milMonths2016[3] * 10.0).round / 10.0
+    @milMonths2016[4] = ( @milMonths2016[4] * 10.0).round / 10.0
+    @milMonths2016[5] = ( @milMonths2016[5] * 10.0).round / 10.0
+    @milMonths2016[6] = ( @milMonths2016[6] * 10.0).round / 10.0
+    @milMonths2016[7] = ( @milMonths2016[7] * 10.0).round / 10.0
+    @milMonths2016[8] = ( @milMonths2016[8] * 10.0).round / 10.0
+    @milMonths2016[9] = ( @milMonths2016[9] * 10.0).round / 10.0
+    @milMonths2016[10] = ( @milMonths2016[10] * 10.0).round / 10.0
+    @milMonths2016[11] = ( @milMonths2016[11] * 10.0).round / 10.0
 
     #at this point @milMonths is now rounded to 1 precision point
   end
