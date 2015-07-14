b8e6bfa98bf190ef0aec5af976e31550ab468051
safarsi <safarsi@ucsd.edu>
2015-07-13 17:50:32 -0700

    minor fix needed for radio buttons/ they are still not working

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -1,9 +1,13 @@
 #accessing all opportunities and saving it in oppties variable for use in html
+require 'action_view'
+
+include ActionView::Helpers::DateHelper
 
 class BrowseController < ApplicationController
   def index
+    #get all Oppty objects from database into the @oppties collection
     @oppties = Oppty.all
-    #@opptySearch = Oppty.where(["opptyName LIKE ?", "%#{params[:search]}%"])
+    
     @done=UserOppty.where(user_id:session[:user_id]).where(status:0).joins(:oppty).includes(:oppty)
     @doing=UserOppty.where(user_id:session[:user_id]).where(status:1).joins(:oppty).includes(:oppty)
     @to_do=UserOppty.where(user_id:session[:user_id]).where(status:2).joins(:oppty).includes(:oppty)
@@ -17,12 +21,32 @@ class BrowseController < ApplicationController
     
     @original = browse_index_path
 
+
+
+
+
+
+
+    # UserOppty must change to Oppty
+
+    @today = Time.new
+    @oppties.each do |o|
+      @rfpDate = o.proposalDueDate
+      puts @rfpDate
+      @dayDiff = distance_of_time_in_words(@today, @rfpDate).to_i
+      if @dayDiff <= 30
+        @within30days=UserOppty.where(user_id:session[:user_id]).joins(:oppty).includes(:oppty)
+      elsif @dayDiff <= 60
+       @within60days=UserOppty.where(user_id:session[:user_id]).joins(:oppty).includes(:oppty)
+      elsif @dayDiff <= 90 
+        @within90days=UserOppty.where(user_id:session[:user_id]).joins(:oppty).includes(:oppty)
+      end
+    end
   end
 
   def search
-     @oppties = Oppty.where(["opptyName LIKE ?", "%#{params[:search]}%"])
+    @oppties = Oppty.where(["opptyName LIKE ?", "%#{params[:search]}%"])
 
-     #@opptySearch = Oppty.where(["opptyName LIKE ?", "%#{params[:search]}%"])
     @done=UserOppty.where(user_id:session[:user_id]).where(status:0).joins(:oppty).includes(:oppty)
     @doing=UserOppty.where(user_id:session[:user_id]).where(status:1).joins(:oppty).includes(:oppty)
     @to_do=UserOppty.where(user_id:session[:user_id]).where(status:2).joins(:oppty).includes(:oppty)
@@ -33,5 +57,22 @@ class BrowseController < ApplicationController
         @none=UserOppty.where(user_id:session[:user_id]).joins(:oppty).includes(:oppty)
       end
     end
+
+
+
+
+
+    # UserOppty must change to Oppty
+    
+    @today = Time.new
+    @oppties.each do |o|
+      @rfpDate = o.proposalDueDate
+      @dayDiff = distance_of_time_in_words(@today, @rfpDate).to_i
+      if @dayDiff <= 30 then @within30days=UserOppty.where(user_id:session[:user_id]).joins(:oppty).includes(:oppty)
+      elsif @dayDiff <= 60 then @within60days=UserOppty.where(user_id:session[:user_id]).joins(:oppty).includes(:oppty)
+      elsif @dayDiff <= 90 then @within90days=USerOppty.where(user_id:session[:user_id]).joins(:oppty).includes(:oppty)
+      end
+    end
   end
+
 end
