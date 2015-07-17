d13803b9e039390fe5e1367fb739a0a993f93110
Richard Tran <RichardTran93@gmail.com>
2015-07-17 07:41:34 -0700

    added numWriters, defaults to 0

--- app/controllers/crm_controller.rb
+++ app/controllers/crm_controller.rb
@@ -452,6 +452,7 @@ class CrmController < ApplicationController
         @oppty=Oppty.new
         #fields
         @oppty.opptyId                = id
+        @oppty.numWriters             = 0
         @oppty.opptyName              = name
         @oppty.idiqCA                 = idiqCA
         @oppty.status2                = status2
