e2fb76085871f62c5474e9b930da65700520bded
Ryan Milkovits <mrmonkey_888@yahoo.com>
2015-07-17 10:20:07 -0700

    db and browse changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,14 +13,14 @@ Oppty.create(opptyId: "1", opptyName:"test", idiqCA:"test", status2:"test", valu
   captureMgr:"test", programMgr:"test",  proposalMgr:"test", technicalLead:"test",sslOrg:"test", slArch:"test",
   ed:"test", on:"test", ate:"test", slComments:"test", rfpDate:"2015-07-01", awardDate:"2015-07-01",
   submitDate:"2015-07-01",  proposalDueDate:"2015-07-06", slDir:"testDir", leadEstim:"testLeadEst",
-  engaged:"testEngaged", solution:"testSolution", estimate:"testEstimate")
+  engaged:"testEngaged", solution:"testSolution", estimate:"testEstimate", numWriters:0 )
 Oppty.create(opptyId: "1", opptyName:"test1", idiqCA:"test1", status2:"test1", value:1, pWin:80,
              captureMgr:"test1", programMgr:"test1",  proposalMgr:"test11", technicalLead:"test1",sslOrg:"test",
              slArch:"test1",ed:"test1", on:"test1", ate:"test", slComments:"test1", rfpDate:"2015-07-01", awardDate:"2015-07-01",
              submitDate:"2015-07-01", proposalDueDate:"2015-07-07", slDir:"testDir1", leadEstim:"testLeadEst1",
-             engaged:"testEngaged1", solution:"testSolution1", estimate:"testEstimate1")
+             engaged:"testEngaged1", solution:"testSolution1", estimate:"testEstimate1", numWriters:0 )
 Oppty.create(opptyId: "1", opptyName:"test2", idiqCA:"test2", status2:"test2", value:1, pWin:80,
              captureMgr:"test2", programMgr:"test2",  proposalMgr:"test2", technicalLead:"test2",sslOrg:"test2",
              slArch:"test2",ed:"test2", on:"test2", ate:"test2", slComments:"test2", rfpDate:"2015-07-01", awardDate:"2015-07-01",
              submitDate:"2015-07-01", proposalDueDate:"2015-07-08", slDir:"testDir2", leadEstim:"testLeadEst2",
-             engaged:"testEngaged2", solution:"testSolution2", estimate:"testEstimate2")
+             engaged:"testEngaged2", solution:"testSolution2", estimate:"testEstimate2", numWriters:0 )
