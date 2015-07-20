96f8819902e8b5e7f8dfa4f8ffdff0edc38ab70e
Kevin Gu <kevingu@Charless-MacBook-Pro.local>
2015-07-20 16:50:24 -0700

    made changes to history

--- app/controllers/allocated_tasks_controller.rb
+++ app/controllers/allocated_tasks_controller.rb
@@ -7,18 +7,9 @@ class AllocatedTasksController < ApplicationController
     Oppty.all.each do |o|
       puts "Count: "+o.user_oppties.count.to_s
       o.user_oppties.each do|uo|
-        if uo.status==1
-          @doing.put(o)
-          break
-        end
-        if uo.status==2
-          @todo.put(o)
-          break
-        end
-        if uo.status==3
 
-        end
       end
+
     end
 
   end
