40a550ecefe54c6fca2f704eb1a091c595a6a4f1
Richard Tran <RichardTran93@gmail.com>
2015-07-21 11:55:33 -0700

    changed approach, handles calculations in script

--- bin/dateExtract.py
+++ bin/dateExtract.py
@@ -1,15 +1,23 @@
+from datetime import datetime, timedelta
 import xlrd
 import sys
-from editpyxl import Workbook
-import ast
-from openpyxl import load_workbook
-import json
+from datetime import datetime
 
+args = sys.argv
+file1 = args[1]
+file2 = args[2]
 
-args = sys.argv # get command line args
-wb = Workbook()
-fileName = args[1]
-wb.open(fileName)
-ws = wb.get_sheet_by_name('Admin')
+book = xlrd.open_workbook(filename=file1)
+sheet = book.sheet_by_name('Admin')
 
-print ws.cell("M2").value
+date = sheet.cell_value(1,12)
+datetime_value = datetime(*xlrd.xldate_as_tuple(date, 0))
+date1 = datetime_value.date()
+
+book = xlrd.open_workbook(filename=file2)
+sheet = book.sheet_by_name('Admin')
+date = sheet.cell_value(1,12)
+datetime_value = datetime(*xlrd.xldate_as_tuple(date, 0))
+date2 = datetime_value.date()
+
+print (date2 - date1).days
