67476cd73a8ea6fbdc52e2540de605ad2e9ee62a
Ryan Milkovits <mrmonkey_888@yahoo.com>
2015-07-21 14:54:29 -0700

    fixed user security url issue, routes also fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,16 +27,14 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    
+    if @user.save
+      puts 'saved'
+    else
+      puts ' not saved'
     end
+
+    redirect_to sessions_new_path
   end
 
   # PATCH/PUT /users/1
--- config/routes.rb
+++ config/routes.rb
@@ -37,6 +37,8 @@ Rails.application.routes.draw do
 
   get 'sessions/destroy'
 
+  post 'users/new'
+
 
   resources :histories
 
@@ -44,7 +46,7 @@ Rails.application.routes.draw do
 
   resources :oppties
 
-  resources :users
+  resources :users, :only => [:new, :create] 
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
