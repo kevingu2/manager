8381166a6e96776d89a2c2bea27505d64e0cfaf1
Richard Tran <RichardTran93@gmail.com>
2015-07-27 14:00:45 -0700

    fixed cell editor hopefully

--- bin/cellEditor.py
+++ bin/cellEditor.py
@@ -1,9 +1,6 @@
-import xlrd
 import sys
-from editpyxl import Workbook
-import ast
 from openpyxl import load_workbook
-import json
+from openpyxl import Workbook
 ######################USAGE######################
 # python cellEditor.py [fileName] "[['id', 'columnName', 'dataToChange'], ..., ['id1', 'columnName1', 'dataToChange1']]"
 #                                   list of list, in string format
@@ -13,7 +10,6 @@ def findRows(fileName, ids):
     ws = wb['PipelineView']
     count = 1
     rows = []
-    print fileName, ids
     for i in ids:
         count = 1
         for row in ws.rows: # search through all rows for the unique ID
@@ -32,11 +28,10 @@ def findCols(cols):
     return columns
 
 args = sys.argv # get command line args
-wb = Workbook()
 fileName = args[1]
 strs = args[2]
 data = [[i for i in x.strip(" []").split(", ")] for x in strs.strip('[]').split("],")] # magic
-wb.open(fileName)
+wb = load_workbook(filename = fileName, keep_vba=True)
 ws = wb.get_sheet_by_name('PipelineView')
 #####get the rows/cols/valuesToChange#########
 ids = []
@@ -50,6 +45,6 @@ rows = findRows(fileName, ids)
 cols = findCols(cols)
 #print len(cols), len(rows)
 for c in range(len(changes)): # make the changes
-    ws.cell(str(cols[c]) + str(rows[c])).value = changes[c]
+    ws[str(cols[c]) + str(rows[c])]= changes[c]
+    print "changed: " + str(cols[c]) + str(rows[c])
 wb.save(fileName)
-#wb.close()
