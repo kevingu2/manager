31e3f6f80d282dd72257c207ed8d35eee3989a39
Richard Tran <rntran@ucsd.edu>
2015-07-31 17:56:14 -0500

    download file is now accurate, i hope

--- bin/recreateExcel.py
+++ bin/recreateExcel.py
@@ -1,21 +1,39 @@
+import tempfile
 import zipfile
-import sys
+from shutil import move, rmtree
 import os
+import sys
+
+def remove_from_zip(zipfname, *filenames):
+    tempdir = tempfile.mkdtemp()
+    try:
+        tempname = os.path.join(tempdir, 'new.zip')
+        with zipfile.ZipFile(zipfname, 'r') as zipread:
+            with zipfile.ZipFile(tempname, 'w') as zipwrite:
+                for item in zipread.infolist():
+                    if item.filename not in filenames:
+                        data = zipread.read(item.filename)
+                        zipwrite.writestr(item, data)
+        move(tempname, zipfname)
+    finally:
+        rmtree(tempdir)
 
 args = sys.argv
-excelFile = args[1]
+excelFileName = args[1]
 sharedStringsDir = args[2]
 sheetDir = args[3]
-
+sharedStringName = sharedStringsDir.replace('public/uploads/data/', '')
+sheetName = sheetDir.replace('public/uploads/data/', '')
 #rename .xlsm to .zip
-zipFileName = excelFileName.replace('.xlsm', 'zip')
-os.rename(excelFileName, zipFileName)
+zipFileName = excelFileName.replace('.xlsm', '.zip')
+move(excelFileName, zipFileName)
 
-# add the files to the zip file
-z = zipfile.ZipFile(excelFileName, "a") # a for append
-z.write(sharedStringsDir)
-z.write(sheetDir)
-z.close()
+#remove the two files from the zip
+remove_from_zip(zipFileName, 'xl/sharedStrings.xml', 'xl/worksheets/sheet2.xml')
+with zipfile.ZipFile(zipFileName, 'a') as z:
+    z.writestr(sharedStringName, open(sharedStringsDir).read())
+    z.writestr(sheetName, open(sheetDir).read())
+    print z.printdir()
+    z.close()
 
-#rename zip file back to excel
-os.rename(zipFileName, excelFileName)
+move(zipFileName, excelFileName)
