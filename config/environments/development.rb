36c73e6c1e167a53f59baad1f90ed20eca6779a1
Kevin Gu <kevingu@Charless-MBP.us-sd-pt-loma-technologie.saic.com>
2015-07-31 16:19:21 -0700

    added javascript for the delete oppties on writer end

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,6 +44,7 @@ Rails.application.configure do
   config.assets.precompile += %w( jquery.growl.js)
   config.assets.precompile += %w( Sortable.js)
   config.assets.precompile += %w( updateStatus.js)
+  config.assets.precompile += %w( tasks.js)
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
