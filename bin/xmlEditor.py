733bbc25e4e770dde027a11e20b55e7c5e268ff6
Richard Tran <rntran@ucsd.edu>
2015-08-03 16:52:00 -0500

    optimized regex speed

--- bin/xmlEditor.py
+++ bin/xmlEditor.py
@@ -61,8 +61,8 @@ sharedStringsDir = args[1]
 sheetDir = args[2]
 strs = args[3]
 # open file and find all of the input strings
-sharedStrings = open(sharedStringsDir, 'r').read()
-sheet = open(sheetDir).read() # this is the PipelineView sheet
+sharedStrings = open(sharedStringsDir, 'rb').read()
+sheet = open(sheetDir, 'rb').read() # this is the PipelineView sheet
 excelValues = pickle.load(open('public/uploads/data/coordinateToValue', 'rb')) # load dictionary of {cellCoordinate : cellValue}
 
 # get all of the pairs in the files
@@ -70,10 +70,10 @@ matches = tuple(re.finditer(r'<t>(.*)</t>', sharedStrings, re.M|re.I))
 stringMatches = [m.group(1) for m in matches]#current groups are [<t>] [whatwewant] [</t>], this keeps only what we want
 
 
-# matches = tuple(re.finditer(r'<c r="(.*)" s="\d*" t="(.*)">\s*<v>(.*)</v>', sheet, re.M|re.I)) # magical regex I wrote. Seriously
-# cellMatches = {}
-# for m in matches:
-#     cellMatches[m.group(1)] = m.group(3) # group 1 is the cell, group 3 is the corresponding sharedStrings number
+matches = tuple(re.findall(r'<c r="(\w*)" s="\d*" t="(\w*)">\s*<v>(\d*)</v>', sheet, re.M|re.I)) # magical regex I wrote. Seriously
+cellMatches = {}
+for m in matches:
+    cellMatches[m.group(1)] = m.group(3) # group 1 is the cell, group 3 is the corresponding sharedStrings number
 
 
 data = [[i for i in x.strip(" []").split(", ")] for x in strs.strip('[]').split("],")] # magic
