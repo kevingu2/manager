a3771547e50de8f1a700d3abf04683151954a761
safarsi <safarsi@ucsd.edu>
2015-08-04 14:34:52 -0700

    invalid data page

--- app/controllers/invalid_data_controller.rb
+++ app/controllers/invalid_data_controller.rb
@@ -1,5 +1,9 @@
 class InvalidDataController < ApplicationController
   def index
-    @invalid_data=Oppty.where("rfpDate<?", Date.today.to_s)
+    @invalid_data=Oppty.where(["opptyName LIKE ? and rfpDate<?", "%#{params[:search]}%", Date.today.to_s]).order(params[:sort]).page(params[:page]).per_page(15)
+  end
+
+  def search
+  	@invalid_data=Oppty.where(["opptyName LIKE ? and rfpDate<?", "%#{params[:search]}%", Date.today.to_s]).order(params[:sort]).page(params[:page]).per_page(15)
   end
 end
