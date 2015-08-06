a0030f368ac13aa43dff0ff65a082aeed6c46bc8
Ryan Milkovits <mrmonkey_888@yahoo.com>
2015-08-06 11:09:02 -0700

    merge total fix

--- app/controllers/invalid_data_controller.rb
+++ app/controllers/invalid_data_controller.rb
@@ -1,5 +1,11 @@
 class InvalidDataController < ApplicationController
-  def index
-    @invalid_data=Oppty.where("rfpDate<?", Date.today.to_s)
-  end
+ def index
+     if params[:search]
+       @invalid_data=Oppty.where(["opptyName LIKE ? and rfpDate<?", "%#{params[:search]}%", Date.today.to_s]).order(params[:sort]).page(params[:page]).per_page(15)
+     else
+         @invalid_data=Oppty.where(["rfpDate<?", Date.today.to_s]).order(params[:sort]).page(params[:page]).per_page(15)
+     end
+
+ end
+
 end
