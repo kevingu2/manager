eebcc668270bf6f1ad22ad7eb9ff68a55012a4a8
Ryan Milkovits <mrmonkey_888@yahoo.com>
2015-08-11 09:02:37 -0700

    sessions revert

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,43 @@
+# TO DO:
+#    add roles : "estimators" and "directors" on lines 23 and 26
+
+#login page
+
 class SessionsController < ApplicationController
+ 
 	skip_before_action :authorize , only: [:destroy,:new, :create]
 	def new
-		#redirect_to root_path if current_user
-	end
-
-	def create
-		ldap_user = Adauth.authenticate(params[:username], params[:password])
-		if ldap_user
-			puts '----- user authenticated -----'
-        	user = User.return_and_create_from_adauth(ldap_user)
-        	session[:user_id] = user.id
-        	redirect_to allocated_tasks_index_path
-    	else
-    		puts '----- user not authenticated -----'
-        	redirect_to brow_index_path
-    	end
-	end
-
-	def destroy
 		session[:user_id] = nil
-		redirect_to root_path
 	end
+  
+	#finds the user in the database (array of users)
+	def create
+		user = User.find_by(name: params[:username])
+		if user and user.authenticate(params[:password])
+			#these info are being used in each session
+			session[:user_id] = user.id
+			session[:username]=user.name
+			session[:role]=user.role
+		
+			#redirecting to desired pages according to the role
+			if user.role=="writer"
+				redirect_to tasks_index_path
+			end
+
+			if user.role=="manager"
+				redirect_to allocated_tasks_index_path
+			end
+		else
+			redirect_to sessions_new_path, notice: "Invalid user/password combination"
+		end
+	end
+ 
+  #log out
+  def destroy
+    session[:user_id] = nil
+    session[:username]=nil
+    session[:role]=nil
+    session[:remember_me]=nil
+    redirect_to sessions_new_path, notice: "Logged out"
+  end
 end
