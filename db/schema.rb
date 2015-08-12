801a4b305421da9c76033e8ffd6a8f42afebd4b2
safarsi <safarsi@ucsd.edu>
2015-08-12 10:00:17 -0700

    x

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(version: 20150812153421) do
     t.datetime "updated_at"
   end
 
+  create_table "browses", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "histories", force: true do |t|
     t.string   "opptyId"
     t.string   "opptyName"
@@ -262,7 +267,7 @@ ActiveRecord::Schema.define(version: 20150812153421) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "status"
+    t.integer  "status",     default: 2
     t.boolean  "changeRFP"
   end
 
