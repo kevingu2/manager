45836a5e6516271c7559a04c29b805d725e2a5fd
Kevin Gu <kevingu@Charless-MBP.us-sd-pt-loma-technologie.saic.com>
2015-08-19 16:37:34 -0700

    fixed crm edit, moving oppties to histories

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   before_action :authorize
   protect_from_forgery with: :exception
+  CRM_PATH = File.join(Rails.root, "public", "uploads")
+  helper_method :deleteUnUnploadedFile
   
   #depending on user's role, they can access/update certain information
   protected
@@ -26,5 +28,29 @@ class ApplicationController < ActionController::Base
       end
     end
   end
-
+  def deleteUnUnploadedFile
+    if Dir[CRM_PATH + '/*.xlsm'].length > 1 #if there is more than one file, delete the old one. else the new overwrote the old, don't delete
+      earliest_file_name=""
+      earliest_date=Time.new
+      Dir[CRM_PATH + '/*.xlsm'].each do |item|
+        next if item == '.' or item == '..'
+        puts "*"*30
+        puts item
+        puts "*"*30
+        puts "creation Time: "+File.ctime(item).to_s
+        puts "Earliest_time: "+earliest_date.to_s
+        if (File.ctime(item)<earliest_date)
+          earliest_file_name=item
+          earliest_date=File.ctime(item)
+        end
+      end
+      puts "*"*30
+      puts earliest_file_name
+      puts earliest_date
+      puts "*"*30
+      FileUtils.mv(earliest_file_name,   File.join(Rails.root, "public", File.basename(earliest_file_name)))
+      FileUtils.rm_rf(Dir.glob(CRM_PATH+'/*'))
+      FileUtils.mv(File.join(Rails.root, "public", File.basename(earliest_file_name)),earliest_file_name)
+    end
+  end
 end
