6539057957b48fa03c7ef559a874e54776616901
safarsi <safarsi@ucsd.edu>
2015-08-31 09:07:50 -0700

    style in edit and assign page

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(version: 20150828185526) do
     t.datetime "updated_at"
   end
 
+  create_table "browses", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "histories", force: true do |t|
     t.string   "opptyId"
     t.string   "opptyName"
@@ -294,20 +299,7 @@ ActiveRecord::Schema.define(version: 20150828185526) do
     t.string   "role"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "remember_digest"
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.integer  "new_notif_count",        default: 0
+    t.integer  "new_notif_count", default: 0
   end
-
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
