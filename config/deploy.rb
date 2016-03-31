bc8fab7d848e1d4234b3f89deef93f2ca398d4fe
kevingu <kevingu2@yahoo.com>
2016-03-30 18:27:31 -0700

    fixed login img path

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,6 +11,7 @@ set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public
 set :keep_releases, 5
 set :rvm_type, :user
 set :rvm_ruby_version, 'ruby-2.3.0' # Edit this if you are using MRI Ruby
+set :assets_roles, [:web, :app]
 
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
